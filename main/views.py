30479bf6cd16eab0593dffd88c6ae830d357db88
aryawijayak <aryawijayak2004@gmail.com>
2023-09-18 19:01:08 +0700

    Tutorial 2 JSON

--- main/views.py
+++ main/views.py
@@ -1,10 +1,29 @@
+from django.http import HttpResponseRedirect
+from main.forms import ProductForm
+from django.urls import reverse
 from django.shortcuts import render
+from main.models import Product
+
 
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
-        'name': 'Arya Wijaya Kusuma',
-        'class': 'PBP C'
+        'name': 'Pak Bepe', # Nama kamu
+        'class': 'PBP A', # Kelas PBP kamu
+        'products': products
     }
 
     return render(request, "main.html", context)
+
 # Create your views here.
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
